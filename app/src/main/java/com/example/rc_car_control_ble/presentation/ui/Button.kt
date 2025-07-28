cf33c3e74fd3134460850e816a524b22590d5073
Patrik Mlcoch <patrik.mlcoch@gmail.com>
2025-07-28 21:14:36 +0200

    Manifest bluetooth permisions, project structure

--- app/src/main/java/com/example/rc_car_control_ble/presentation/ui/Button.kt
+++ app/src/main/java/com/example/rc_car_control_ble/presentation/ui/Button.kt
@@ -1,10 +1,8 @@
-package com.example.rc_car_control_ble.ui
+package com.example.rc_car_control_ble.presentation.ui
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedIconButton
