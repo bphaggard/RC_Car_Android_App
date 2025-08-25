6f7f2e886de6ec34178f4bc7f65d8941ab4e42ad
Patrik Mlcoch <patrik.mlcoch@gmail.com>
2025-08-25 21:33:02 +0200

    update BluetoothUIState, viewModel

--- app/src/main/java/com/example/rc_car_control_ble/domain/BluetoothUiState.kt
+++ app/src/main/java/com/example/rc_car_control_ble/domain/BluetoothUiState.kt
@@ -3,4 +3,7 @@ package com.example.rc_car_control_ble.domain
 data class BluetoothUiState(
     val scannedDevices: List<BluetoothDevice> = emptyList(),
     val pairedDevices: List<BluetoothDevice> = emptyList(),
+    val isConnected: Boolean = false,
+    val isConnecting: Boolean = false,
+    val errorMessage: String? = null
 )
